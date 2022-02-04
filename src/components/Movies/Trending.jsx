916b02f0a8a2f71660d4fa0d5cb37247953994c4
KareConnect <tapesh.patel@kare.ie>
2022-02-04 12:01:02 +0000

    added more functionality to extend TV component

---
+++ src/components/Movies/Trending.jsx
@@ -0,0 +1,167 @@
+import React ,{useEffect, useState, useContext} from 'react';
+import {Grid, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography, makeStyles } from '@material-ui/core';
+import Rating from '@material-ui/lab/Rating';
+import "./Movies.css"
+import NotFound from './NotFound';
+import { wList } from '../../Context.js';
+
+
+
+
+const useStyles = makeStyles (theme => ({
+  cardButton:{
+      '&:hover':{
+         backgroundColor:'#356E44',
+         color: 'white',
+      }
+  },
+
+  Main:{
+      width: "100%",
+      display: "flex",
+      flexWrap: "wrap",
+      alignItems: "center", 
+      alignContent: "center",
+      justifyContent: "flex-start;",
+      columnGap: "2.5rem",
+      rowGap:"5px",
+  },
+  cardMain:{
+      width: '200px',
+      height: '18rem',
+      margin: '10px',
+      '&:hover':{
+          boxShadow: '5px 3px 5px gray',
+       }
+  },
+  cardContent: {
+      width: '95%',
+      height: '50px',
+      padding: '5px',
+      '& .MuiTypography-body1': {
+          fontSize: '13px',
+          wordBreak: 'break-word',
+          fontWeight: 'bolder',
+      },
+
+  },
+  movieTitle:{
+      textOverflow: 'ellipsis',
+      overflow: 'hidden',
+      height: '1.5rem',
+      marginBottom: '5px',
+
+  },
+  typography1:{
+      float: 'right',
+      '&.MuiTypography-body2':{
+          fontSize: '13px',
+      },  
+  },
+  typography2:{
+      float: 'left',
+      '&.MuiRating-root':{
+          color: 'red',
+          fontSize: '13px',
+      },
+  },
+}));
+
+const Trending = ({
+  searchText, 
+}) => {
+
+  const classes = useStyles();
+
+    
+    const [movieData, setMovieData] = useState([]);
+    const {watchlist, setWatchlist, page , setNumberOfPages} = useContext(wList);
+    
+    const addToWatchlist = (movieData) => {
+            const addItem = [...watchlist, movieData]
+            setWatchlist(addItem);   
+        }
+
+      
+
+
+        useEffect (()=> {
+          const getMovieList  = async () => {
+
+              const TrendingUrl = `https://api.themoviedb.org/3/trending/${window.location.pathname === "/movies" ? "movie" : "tv"}/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+            //  const movieSearchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${searchText}&page=${page}`
+              // const discoverMovieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${gUrl}`
+                 //const tresponse = await fetch (searchText ===""? movieTrendingUrl : searchtvUrl);
+
+              const response = await fetch (  TrendingUrl  );
+              try {
+                  const responseJson = await response.json();
+                  const data = (responseJson.results);
+                  const pages = (responseJson.total_pages)
+                  setMovieData(data);
+                  setNumberOfPages(pages);
+                  } catch (err) {
+                  console.error(err);
+              }                
+          };
+  
+              getMovieList();  
+              
+            // eslint-disable-next-line
+          }, [ page]);
+
+  return (<>
+            <Grid
+                item
+                xs
+                container
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+                style={{ backgroundColor: "black", width: "100%", height: "100%"}}
+            >
+                    <div  className ={classes.Main} > 
+                    { movieData.length === 0 ? <NotFound notfound = {searchText} /> : movieData.map((movie) =>{
+                        return(
+                            <Card className={classes.cardMain}  key={movie.id}>
+                          
+                        <CardActionArea>
+                            <CardMedia 
+                                height = "190"
+                                component = "img"
+                                className="cardImage"
+                                image = {`https://image.tmdb.org/t/p/original/${movie.poster_path}`} 
+                                alt = "movie poster"
+                                title = {movie.media_type === "movie"? movie.title : movie.name}
+                            />
+                            <CardContent className = {classes.cardContent}>
+                                <Typography className = {classes.movieTitle}>  {movie.media_type === "movie"? movie.title : movie.name} </Typography>
+                                <Typography 
+                                            className = {classes.typography1} 
+                                            variant="body2" 
+                                            component = "p"
+                                    > {movie.media_type === "movie"? movie.release_date : movie.first_air_date} 
+                                    </Typography>
+                                <Rating 
+                                        className = {classes.typography2} 
+                                        name = "ratings"
+                                        value =  {movie.vote_average/2} 
+                                        precision={0.5}
+                                        readOnly                                
+                                />                             
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions style = {{justifyContent: 'space-evenly'}} >
+                            <Button className = {classes.cardButton} size = "small">Watch</Button>
+                            <Button className = {classes.cardButton} size = "small" >Share</Button>
+                            <Button className = {classes.cardButton}size = "small" onClick = {   ()=> addToWatchlist(movie) }> Add </Button> 
+                        </CardActions>      
+                    </Card>
+                        );                    
+                    })}            
+                    </div>
+            </Grid> 
+  </>);
+};
+
+export default Trending;
