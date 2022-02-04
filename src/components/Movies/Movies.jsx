db5f5668813be71a793aa1c958b67948d3828fee
KareConnect <tapesh.patel@kare.ie>
2022-02-04 11:49:05 +0000

    added video player

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,14 @@
 import React ,{useEffect, useState, useContext} from 'react';
-import {Grid, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography, makeStyles } from '@material-ui/core';
+import {Grid, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography, makeStyles} from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
+import "./Movies.css"
 import NotFound from './NotFound';
 import { wList } from '../../Context.js';
 import PageNav from './PageNav.jsx';
-
+import FilterNav from './FilterNav';
+import useGenres from '../../hooks/useGenres';
+import Trending from './Trending'
+import VideoModal from './VideoModal'
 
 
 
@@ -32,7 +36,7 @@ const useStyles = makeStyles (theme => ({
     },
     cardMain:{
         width: '200px',
-        height: '260px',
+        height: '18rem',
         margin: '10px',
         '&:hover':{
             boxShadow: '5px 3px 5px gray',
@@ -69,29 +73,23 @@ const useStyles = makeStyles (theme => ({
             fontSize: '13px',
         },
     },
-    filtermain:{
-        color: 'white',
-        width: "98%",
-        margin: "auto",
-        fontWeight: 'bold',
-        display: 'flex',
-        justifyContent: 'space-between',
-        backgroundColor: 'white',
-        border: '1px solid green',
-        borderRadius: '5px',
-    },
 }));
 
 
 const Movies = (props) =>{
     
     const classes = useStyles();
+
     
     const [movieData, setMovieData] = useState([]);
     const {watchlist, setWatchlist, page, setPage, numberOfPages , setNumberOfPages} = useContext(wList);
-
-
-    
+    const [genres, setGenres] = useState([])
+    const [selectedGenres, setSelectedGenres] = useState([])
+    const [watchVideo, setWatchVideo] = useState(false)
+    const [movieVideoId, setMovieVideoId ] = useState([])
+    const [showTrending, setShowTrending] = useState(false)
+    const gUrl  = useGenres(selectedGenres)
+  
     
 
     const filterData = ((val) => {
@@ -106,17 +104,25 @@ const Movies = (props) =>{
     
     const addToWatchlist = (movieData) => {
             const addItem = [...watchlist, movieData]
-            setWatchlist(addItem);   
+            setWatchlist(addItem);    
         }
-    
+        const handleWatchOpen = (movied) => {
+            setMovieVideoId(movied)
+        }
+        const handleWatchClose =() => {
+            setWatchVideo(false)
+            setMovieVideoId("")
+        }
     
     useEffect (()=> {
         const getMovieList  = async () => {
-        
+         // eslint-disable-next-line
             const movieTrendingUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
             const movieSearchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${props.searchText}&page=${page}`
-    
-            const response = await fetch (props.searchText ===""? movieTrendingUrl : movieSearchUrl);
+            const discoverMovieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${gUrl}`
+               //const tresponse = await fetch (props.searchText ===""? movieTrendingUrl : searchtvUrl);
+            
+            const response = await fetch ( (props.searchText ==="" && selectedGenres? discoverMovieUrl : movieSearchUrl)  );
             try {
                 const responseJson = await response.json();
                 const data = (responseJson.results);
@@ -131,8 +137,12 @@ const Movies = (props) =>{
             getMovieList();  
             
           // eslint-disable-next-line
-        }, [props.searchText, page]);
+        }, [props.searchText,  gUrl,  page]);
         
+        useEffect(() => {
+            setPage(1);
+        }, [showTrending, setPage]);
+    
         return (
             <> 
             <Grid
@@ -144,59 +154,63 @@ const Movies = (props) =>{
                 alignItems="flex-start"
                 style={{ backgroundColor: "black", width: "100%", height: "100%"}}
             >
-                <div className={classes.filtermain}>
-                    <div className={classes.filterinner1}>
-                        <Button className={classes.frecentlyadded}>Recently Added </Button>
-                        <Button className={classes.fmostpopular}>Most Popular</Button>
+                <FilterNav  
+                genres = {genres}
+                setGenres = {setGenres}
+                selectedGenres = {selectedGenres}
+                setSelectedGenres = {setSelectedGenres}
+                setPage = {setPage}
+                setShowTrending = {setShowTrending}
+                showTrending = {showTrending}
+                /> 
+
+                {
+                    showTrending? <Trending searchText = {props.searchText}  />: null
+                  
+                }
+                  <div  className ={classes.Main} > 
+                    { movieData.length === 0 ? <NotFound notfound = {props.searchText} /> : movieData.filter(filterData).map((movie) =>{
+                        return(
+                            <Card className={classes.cardMain}  key={movie.id}>
+                        <CardActionArea>
+                            <CardMedia 
+                                height = "190"
+                                component = "img"
+                                className="cardImage"
+                                image = {`https://image.tmdb.org/t/p/original/${movie.poster_path}`} 
+                                alt = "movie poster"
+                                title = {movie.title}
+                            />
+                            <CardContent className = {classes.cardContent}>
+                                <Typography className = {classes.movieTitle}>  {movie.title} </Typography>
+                                <Typography 
+                                            className = {classes.typography1} 
+                                            variant="body2" 
+                                            component = "p"
+                                    > {movie.release_date} 
+                                    </Typography>
+                                <Rating 
+                                        className = {classes.typography2} 
+                                        name = "ratings"
+                                        value =  {movie.vote_average/2} 
+                                        precision={0.5}
+                                        readOnly                                
+                                />                             
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions style = {{justifyContent: 'space-evenly'}} >
+                            <Button className = {classes.cardButton} size = "small" onClick = {  () => {handleWatchOpen(movie) ;   setWatchVideo(true)} }>Watch</Button>
+                            <VideoModal watchVideo = {watchVideo} handleWatchClose = {handleWatchClose}  movieVideoId = {movieVideoId} />
+                            <Button className = {classes.cardButton} size = "small" >Share</Button>
+                            <Button className = {classes.cardButton}size = "small" onClick = {   ()=> addToWatchlist(movie) }> Add </Button> 
+                        </CardActions>                   
+                    </Card>
+                        );                    
+                    })}      
                     </div>
-                    <div className={classes.filterinner2}>
-                        <Button className={classes.fyear}>Year</Button>
-                        <Button className={classes.fgenre}>Genre</Button>
-                        <Button className={classes.flanguage}>Language</Button>
-                        <Button className={classes.fsortby}>Sort by</Button>
-                    </div>
-                </div>
-               
-                <div  className ={classes.Main} > 
-                { movieData.length === 0 ? <NotFound notfound = {props.searchText} /> : movieData.filter(filterData).map((movie) =>{
-                    return(
-                        <Card className={classes.cardMain}  key={movie.id}>
-                    <CardActionArea>
-                        <CardMedia 
-                            height = "150"
-                            component="img"
-                            image = {`https://image.tmdb.org/t/p/original/${movie.poster_path}`} 
-                            alt = "movie poster"
-                            title = {movie.title}
-                        />
-                        <CardContent className = {classes.cardContent}>
-                            <Typography className = {classes.movieTitle}>  {movie.title} </Typography>
-                            <Typography 
-                                        className = {classes.typography1} 
-                                        variant="body2" 
-                                        component = "p"
-                                > {movie.release_date} 
-                                </Typography>
-                            <Rating 
-                                    className = {classes.typography2} 
-                                    name = "ratings"
-                                    value =  {movie.vote_average/2} 
-                                    precision={0.5}
-                                    readOnly                                
-                            />                             
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions style = {{justifyContent: 'space-evenly'}} >
-                        <Button className = {classes.cardButton} size = "small">Watch</Button>
-                        <Button className = {classes.cardButton} size = "small" >Share</Button>
-                        <Button className = {classes.cardButton}size = "small" onClick = {   ()=> addToWatchlist(movie) }> Add </Button> 
-                    </CardActions>                   
-                </Card>
-                    );                    
-                })}            
-                </div>
-            </Grid> 
-            <PageNav  setPage = {setPage}  numberOfPages = {numberOfPages}/>
+                </Grid> 
+                <PageNav  setPage = {setPage}  page={page} numberOfPages = {numberOfPages}/>
+              
         </>    )
 };
 export default Movies;
